a1bd49d462534c845b09dd007f9175ef8cc5c84d
Renee Li <renee.li@fivetran.com>
2024-10-08 11:53:08 -0400

    add new conversion report tables

---
+++ models/stg_reddit_ads_source__campaign_conversions_report.sql
@@ -0,0 +1,46 @@
+
+with base as (
+
+    select * 
+    from {{ ref('stg_reddit_ads_source__campaign_conversions_report_base') }}
+),
+
+fields as (
+
+    select
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(ref('stg_reddit_ads_source__campaign_conversions_report_base')),
+                staging_columns=get_campaign_conversions_report_columns()
+            )
+        }}
+        {{ fivetran_utils.source_relation(
+            union_schema_variable='reddit_ads_source_union_schemas', 
+            union_database_variable='reddit_ads_source_union_databases') 
+        }}
+    from base
+),
+
+final as (
+    
+    select 
+        source_relation, 
+        _fivetran_synced,
+        account_id,
+        avg_value,
+        campaign_id,
+        click_through_conversion_attribution_window_day,
+        click_through_conversion_attribution_window_month,
+        click_through_conversion_attribution_window_week,
+        date as date_day,
+        event_name,
+        total_items,
+        total_value,
+        view_through_conversion_attribution_window_day,
+        view_through_conversion_attribution_window_month,
+        view_through_conversion_attribution_window_week
+    from fields
+)
+
+select *
+from final
